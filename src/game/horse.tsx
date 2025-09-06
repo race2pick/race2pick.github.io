d3d49d01c1edfcae3dec61072ea8a66a45cedd65
race2pick <race.2.picks@gmail.com>
2025-09-06 21:02:04 +0700

    new horse

--- src/game/horse.tsx
+++ src/game/horse.tsx
@@ -1,31 +1,34 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useArena, playerBoxWidth } from "@/context/arena";
 import { cn } from "@/lib/utils";
-import { type DotLottie, DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { motion, useMotionValue, useMotionValueEvent } from "motion/react";
 import { useEffect, useMemo, useState } from "react";
+import { useLottie } from "lottie-react";
+import horseAmin from "../assets/horse.json";
 
-const anim = new URL("../assets/horse.lottie", import.meta.url).href;
-
-type ThemeKey =
-  | "default"
-  | `${1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13}`;
-
-const colors: Record<ThemeKey, string> = {
-  default: "#121212",
-  "1": "#ffffff",
-  "2": "#6666FF",
-  "3": "#55AFE7",
-  "4": "#24A8AF",
-  "5": "#FF6699",
-  "6": "#E7B549",
-  "7": "#DE524C",
-  "8": "#767FAD",
-  "9": "#FF0000",
-  "10": "#FFFA05",
-  "11": "#0019FF",
-  "12": "#2EFF00",
-  "13": "#00FFED",
-};
+const themes = [
+  "white",
+  "red",
+  "black",
+  "green",
+  "blue",
+  "orchid",
+  "yellow",
+  "magenta",
+  "cyan",
+  "oranye",
+  "chartreuse",
+  "deep-pink",
+  "dark-turquoise",
+  "green-yellow",
+  "orangered",
+  "turquoise",
+  "gold",
+  "lawn-green",
+  "hot-pink",
+  "cornflower-blue",
+  "dodger-blue",
+];
 
 interface HorseProps {
   name: string;
@@ -33,10 +36,8 @@ interface HorseProps {
 }
 
 export default function Horse({ name, index }: HorseProps) {
-  const [dotLottie, setDotLottie] = useState<DotLottie | null>(null);
   const [position, setPosition] = useState(0);
   const [curPosition, setCurPosition] = useState(0);
-  // const [speedNow, setSpeedNow] = useState(0);
 
   const {
     players,
@@ -59,50 +60,46 @@ export default function Horse({ name, index }: HorseProps) {
 
   const x = useMotionValue(0);
 
-  const dotLottieRefCallback = (dotLottie: DotLottie) => {
-    setDotLottie(dotLottie);
-  };
-
   const themeId = useMemo(() => {
-    const themes: ThemeKey[] = [
-      "default",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "11",
-      "12",
-      "13",
-    ];
     const cycleIndex = index % themes.length;
     return themes[cycleIndex];
   }, [index]);
 
+  const {
+    View,
+    play: dotlottiePlay,
+    pause: dotlottiePause,
+    setSpeed: dotlottieSetSpeed,
+    goToAndStop: dotlottieGoToAndStop,
+  } = useLottie({
+    animationData: horseAmin,
+    loop: true,
+    autoplay: false,
+    renderer: "svg",
+    style: { height: `${playerHeight}px`, width: "70px", flexShrink: 0 },
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid meet",
+    },
+  });
+
   useEffect(() => {
     if (gameState === "not-started") {
-      dotLottie?.pause();
-      dotLottie?.setFrame(0);
+      dotlottieGoToAndStop(0);
     }
-  }, [gameState, dotLottie]);
+  }, [gameState]);
 
   useEffect(() => {
     if (gameState === "started") {
-      dotLottie?.play();
+      dotlottiePlay();
       const min = speed[0] * 7;
       const max = speed[1] * 7;
       const boost = randomInt(min, max);
-      const ratio = (boost - min) / (max - min); // normalize ke 0..1
-      const animSpeed = 0.5 + ratio * 0.1 * 1.0; // 0.5..1.5
-      dotLottie?.setSpeed(animSpeed);
+      const ratio = (boost - min) / (max - min);
+      const animSpeed = 0.5 + ratio * 0.1 * 1.0;
+      dotlottieSetSpeed(Math.max(animSpeed, 0.5));
       setPosition(boost);
     }
-  }, [gameState, dotLottie, speed]);
+  }, [gameState, speed]);
 
   useEffect(() => {
     if (gameState !== "started") return;
@@ -111,44 +108,40 @@ export default function Horse({ name, index }: HorseProps) {
     let targetSpeed = randomInt(speed[0], speed[1]);
 
     const interval = setInterval(() => {
-      targetSpeed = randomInt(speed[0], speed[1]); // random tujuan baru
-    }, 1000); // tiap 1 detik ganti target
+      targetSpeed = randomInt(speed[0], speed[1]);
+    }, 1000);
 
     const frame = setInterval(() => {
-      // Smooth menuju target
       speedNow += (targetSpeed - speedNow) * 0.1;
       setPosition((prev) => {
         const next = prev + speedNow;
         return next >= raceLong ? raceLong : next;
       });
 
-      // Update animasi berdasarkan speed
-      if (dotLottie) {
-        const min = speed[0];
-        const max = speed[1];
-        const ratio = (speedNow - min) / (max - min); // normalize ke 0..1
-        const animSpeed = 0.5 + ratio * 1.0; // 0.5..1.5
-        dotLottie.setSpeed(animSpeed);
-      }
-    }, 100); // update tiap 0.1 detik
+      const min = speed[0];
+      const max = speed[1];
+      const ratio = (speedNow - min) / (max - min);
+      const animSpeed = 0.5 + ratio * 1.0;
+      dotlottieSetSpeed(animSpeed);
+    }, 100);
 
     return () => {
       clearInterval(interval);
       clearInterval(frame);
     };
-  }, [dotLottie, gameState, raceLong, speed]);
+  }, [gameState, raceLong, speed]);
 
   useEffect(() => {
     let to: NodeJS.Timeout;
 
     if (gameState === "finished") {
-      dotLottie?.pause();
+      dotlottiePause();
       if (winner === name) {
         setPosition(curPosition + 2);
-        dotLottie?.setFrame(5);
+        dotlottieGoToAndStop(23);
       } else {
         setPosition(curPosition - 3);
-        dotLottie?.setFrame(0);
+        dotlottieGoToAndStop(0);
       }
 
       to = setTimeout(() => {
@@ -157,26 +150,26 @@ export default function Horse({ name, index }: HorseProps) {
     }
 
     return () => clearTimeout(to);
-  }, [curPosition, dotLottie, gameState, name, setGameState, winner]);
+  }, [curPosition, gameState, name, setGameState, winner]);
 
   useEffect(() => {
     let to: NodeJS.Timeout;
     if (gameState === "celebration") {
       setPosition(raceLong + playerBoxWidth);
-      dotLottie?.play();
+      dotlottiePlay();
       to = setTimeout(() => {
         setGameState("end");
       }, 2000);
     }
 
     return () => clearTimeout(to);
-  }, [dotLottie, gameState, raceLong, setGameState]);
+  }, [gameState, raceLong, setGameState]);
 
   useEffect(() => {
     if (gameState === "end") {
-      dotLottie?.pause();
+      dotlottieGoToAndStop(0);
     }
-  }, [dotLottie, gameState]);
+  }, [gameState]);
 
   useEffect(() => {
     if (gameState === "not-started") {
@@ -199,8 +192,7 @@ export default function Horse({ name, index }: HorseProps) {
           return prev;
         });
 
-        dotLottie?.pause();
-        dotLottie?.setFrame(0);
+        dotlottieGoToAndStop(0);
       }
     }
   });
@@ -224,27 +216,21 @@ export default function Horse({ name, index }: HorseProps) {
       }}
     >
       <div
-        className="relative flex justify-end items-center"
+        className={cn(
+          "relative flex justify-end items-center",
+          "horse",
+          themeId
+        )}
         style={{ width: `${playerBoxWidth}px` }}
       >
         <div
           className={cn(
-            "max-w-[130px] truncate overflow-hidden h-auto text-xs rounded-md px-3 mix-blend-difference text-white"
+            "max-w-[130px] truncate overflow-hidden h-auto text-xs rounded-md px-3 mix-blend-difference text-white player-name"
           )}
-          style={{
-            backgroundColor: `color-mix(in srgb, ${colors[themeId]} 25%, transparent)`,
-          }}
         >
           {name}
         </div>
-        <DotLottieReact
-          src={anim}
-          loop
-          dotLottieRefCallback={dotLottieRefCallback}
-          autoplay={false}
-          themeId={themeId}
-          style={{ height: `${playerHeight}px`, width: "70px", flexShrink: 0 }}
-        />
+        {View}
       </div>
     </motion.div>
   );
